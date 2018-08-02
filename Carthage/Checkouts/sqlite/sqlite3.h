544ae9dd33096cd9f8ffb61205c531705b0c59ad
Adonis Peralta <donileo@gmail.com>
2018-08-01 22:17:23 -0400

    Add Sqlite Standalone Scheme
    This scheme allows FMDB to build using the latest sqlite.
    The latest sqlite is brought in as a dependency via carthage.

---
+++ Carthage/Checkouts/sqlite/sqlite3.h
@@ -0,0 +1,19 @@
+//
+//  sqlite.h
+//  sqlite
+//
+//  Created by Adonis Peralta on 8/2/18.
+//  Copyright © 2018 SQLite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <sqlite3/sqlite.h>
+
+//! Project version number for sqlite.
+FOUNDATION_EXPORT double sqliteVersionNumber;
+
+//! Project version string for sqlite.
+FOUNDATION_EXPORT const unsigned char sqliteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sqlite/PublicHeader.h>
+
